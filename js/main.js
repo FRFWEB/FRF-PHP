f76aed10b13f1991f61a0c680de49de377a7abf3
FRFWEB <franciscoglopez2013@gmail.com>
2021-03-07 20:57:50 -0400

    change files

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ DATE: 07-03-2021
 */
 
 /* Animation Index - html */
-gsap.to('.target',{
+gsap.to('.target', {
     rotationX: 360,
     duration: 2
 })
@@ -12,18 +12,18 @@ gsap.to('.target',{
 let menuItems = document.querySelectorAll('#item')
 let panels = document.querySelectorAll('#show_panel')
 menuItems.forEach((items, index) => {
-    items.addEventListener('click',(e)=>{
+    items.addEventListener('click', (e) => {
         e.preventDefault()
-        show(panels,index)
+        show(panels, index)
     })
 });
 
-function show(section,item){
-    section.forEach((element,index) => {
-        if(index === item){
+function show(section, item) {
+    section.forEach((element, index) => {
+        if (index === item) {
             element.classList.add('show')
             element.classList.remove('hide')
-        }else{
+        } else {
             element.classList.add('hide')
             element.classList.remove('show')
         }
@@ -31,38 +31,38 @@ function show(section,item){
 }
 /* Index - html */
 let form = document.getElementById('formUser')
-        form.addEventListener('submit', (e)=>{
-            e.preventDefault();
-            let name = document.getElementById('name').value
-            let email = document.getElementById('email').value
-            let message = document.getElementById('message').value
-            let data = 'name='+name +'&email='+email+'&message='+message
-            let url = 'php/main.php'
-            let status = document.getElementById('result_status')
-            //DEBUG
-            //console.log('name ' + name)
-            //console.log('email ' + email)
-            //console.log('message ' + message)
-            let httpx = new XMLHttpRequest()
-            httpx.onreadystatechange = function(){
-                if(httpx.readyState == 4 && httpx.status == 200){
-                    console.log(httpx.responseText)
-                    let response = httpx.responseText
-                    if(response == 'success'){
-                        status.innerHTML = `
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let message = document.getElementById('message').value
+    let data = 'name=' + name + '&email=' + email + '&message=' + message
+    let url = 'php/main.php'
+    let status = document.getElementById('result_status')
+    //DEBUG
+    //console.log('name ' + name)
+    //console.log('email ' + email)
+    //console.log('message ' + message)
+    let httpx = new XMLHttpRequest()
+    httpx.onreadystatechange = function () {
+        if (httpx.readyState == 4 && httpx.status == 200) {
+            console.log(httpx.responseText)
+            let response = httpx.responseText
+            if (response == 'success') {
+                status.innerHTML = `
                         <div class="target-success">
                             <h5>CREATE USER SUCCESS</h5>
                         </div>`
-                    }else{
-                        status.innerHTML = `
+            } else {
+                status.innerHTML = `
                         <div class="target-error">
                             <h5>ERROR IN CREATE USER</h5>
                         </div>`
-                    }
-                }
             }
-            httpx.open('POST',url,true)
-            httpx.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-            httpx.send(data) 
-            
-        })
+        }
+    }
+    httpx.open('POST', url, true)
+    httpx.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    httpx.send(data)
+
+})
